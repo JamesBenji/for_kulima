ef5163c4656f626ff592030bb47e88b41528f966
James Benjamin Mubiru <jamesbenjamin@gmail.com>
2024-09-28 01:47:46 +0300

    district route fixed

--- app/api/(district_admin)/view-all-parish-admins/route.ts
+++ app/api/(district_admin)/view-all-parish-admins/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
       }
 
       return NextResponse.json({
-        allParishAdmins: response.allParishAdmins,
+        allParishAdmins: response.data,
       }, {status: 200})
 
     } else {
