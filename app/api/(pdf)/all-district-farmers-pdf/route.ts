806ecdb40f1879f292da150f8a50c40c01f65964
James Benjamin Mubiru <jamesbenjamin@gmail.com>
2024-10-18 01:34:00 +0300

    prod-fix-29

--- app/api/(pdf)/all-district-farmers-pdf/route.ts
+++ app/api/(pdf)/all-district-farmers-pdf/route.ts
@@ -115,7 +115,7 @@ async function generateFarmersPDF(farmers: FarmerResponse[]): Promise<Buffer> {
     await page.setContent(html, { waitUntil: "networkidle0" });
 
     const pdfArray = await page.pdf({
-      format: "A4",
+      // format: "A4",
       landscape: true,
       margin: {
         top: "10mm",
